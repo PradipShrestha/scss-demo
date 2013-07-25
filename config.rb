0ff2d75d772ba072ea02241f3c907158f3bc8293
Jay Chand <jchand@bstonetech.com>
2013-07-24 23:22:44 -0400

    New updates

--- config.rb
+++ config.rb
@@ -1,5 +1,21 @@
 # Require any additional compass plugins here.
 
+
+# Change this to :production when ready to deploy the CSS to the live server.
+environment = :development
+#environment = :production
+
+# In development, we can turn on the FireSass-compatible debug_info.
+firesass = false
+#firesass = true
+
+# Require any additional compass plugins installed on your system.
+require 'zen-grids'
+#require 'breakpoint' # https://github.com/canarymason/breakpoint
+#require 'respond-to' # https://github.com/Snugug/respond-to
+#require 'toolkit'    # https://github.com/Snugug/toolkit
+
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
@@ -9,6 +25,7 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :development) ? :expanded : :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
